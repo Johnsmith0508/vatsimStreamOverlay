b6111ef14d329bad2dae0410d3cd8b663f338f42
Logan Waldman <waldmanl@dynalogic.org>
2019-06-11 19:49:44 -0400

    add time online listing

--- webSvr.js
+++ webSvr.js
@@ -41,7 +41,7 @@ app.get("/*",function(req, res) {
     bg1color: req.query.bg1color || "262626",
     bg2color: req.query.bg2color || "3c3c3c"
   };
-  if(req.query.callsign === "" ) {
+  if(req.query.callsign !== undefined ) {
     display.callsign = "Not Connected";
     if (req.path.substring(1) in vatsimParsedData[0]) {
       var atisText = vatsimParsedData[0][req.path.substring(1)].ATIS.split(
@@ -53,18 +53,28 @@ app.get("/*",function(req, res) {
       }
     }
     if (req.path.substring(1) === "0000000"){display.callsign = "London Control"}
-  }else{display.callsign = "";}
-  if(req.query.frequency === "") {
+  }else{display.callsign = "notRequested";}
+  if(req.query.frequency !== undefined) {
     display.frequency = (req.path.substring(1) in vatsimParsedData[0]) ? vatsimParsedData[0][req.path.substring(1)].frequency : "---.---";
     if (display.frequency.endsWith("20")) {display.frequency = display.frequency.replace(/.$/, "5");}
     if (display.frequency.endsWith("70")) {display.frequency = display.frequency.replace(/.$/, "5");}
     if (req.path.substring(1) === "0000000"){display.frequency = "134.900"}
-  }else{display.frequency = "";}
-  if(req.query.position === "") {
+  }else{display.frequency = "notRequested";}
+  if(req.query.position !== undefined) {
     display.position = (req.path.substring(1) in vatsimParsedData[0]) ? vatsimParsedData[0][req.path.substring(1)].callsign : "Not Connected";
     if (req.path.substring(1) === "0000000"){display.position = "LON_D_CTR"}
-  }else{display.position="";}
-  if((req.query.frequency !== "" && req.query.position !== "" && req.query.callsign !== "")){display.error = "noDisplay"}else{display.error=""}
+  }else{display.position="notRequested";}
+  if(req.query.online !== undefined) {
+    if(req.path.substring(1) in vatsimParsedData[0]){
+      var vatTime = vatsimParsedData[0][req.path.substring(1)].connectedSince;
+      var connectedTime =new Date(vatTime.substring(0,4)+"-"+vatTime.substring(4,6)+"-"+vatTime.substring(6,8)+"T"+vatTime.substring(8,10)+":"+vatTime.substring(10,12)+":"+vatTime.substring(10,12)+"Z");
+      var timeOnline = Date.now() - connectedTime;
+      display.online = Math.floor((timeOnline/(1000*60*60))%24).toString().padStart(2,"0") + ":" + Math.floor((timeOnline/(1000*60))%60).toString().padStart(2,"0");
+    }else if (req.path.substring(1) === "0000000"){display.online = "01:35"} else {display.online = "--:--"}
+  }else{display.online = "notRequested"}
+  if(req.query.frequency === undefined && req.query.position === undefined && req.query.callsign === undefined){display.error = "noDisplay"}else{display.error=""}
+
+  //console.log(vatsimParsedData[0][req.path.substring(1)].connectedSince);
   res.render("pages/index", display);
 });
 
@@ -72,10 +82,4 @@ if((Math.floor(new Date() / 1000)) > serverList.timestamp + 604800/*seconds in a
   updateServerList();
 }
 app.listen(8080);
-/*var svr = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var url = req.url.substring(1, req.url.length).split("/");
-  var vatsimData = VatsimInterface.getParsedData(/*url);
-  res.write(url.toString());
-  res.end();
-}).listen(8080);*/
+
